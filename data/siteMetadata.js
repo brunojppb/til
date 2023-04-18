9b389d4d4140eeee34feb96ca25f45b4cee79da8
Bruno Paulino <bruno@bpaulino.com>
2023-04-18 09:26:49 +0200

    Rust ref keyword

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/brunojppb/til',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/twitter-card.jpg',
   email: 'hi@bpaulino.com',
   github: 'https://github.com/brunojppb',
   twitter: 'https://twitter.com/bpaulino0',
--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  printWidth: 100,
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   trailingComma: 'es5',
   bracketSpacing: true,
   overrides: [
     {
-      files: '*.md,*.mdx',
+      files: '*.mdx',
+      options: {
+        proseWrap: 'always',
+      },
+    },
+    {
+      files: '*.md',
       options: {
         proseWrap: 'always',
       },
